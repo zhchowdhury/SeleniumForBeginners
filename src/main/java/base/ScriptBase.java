4675d0f61bae19c82d4bfbc89b598f06c40044a0
Z Chowdhury <zhchowdhurytech@gmail.com>
2020-04-21 00:12:16 -0400

    Hello

--- src/main/java/base/ScriptBase.java
+++ src/main/java/base/ScriptBase.java
@@ -45,7 +45,7 @@ public class ScriptBase {
         else if(browser.equalsIgnoreCase("chromeSauceLab")){
             DesiredCapabilities caps=new DesiredCapabilities();
             caps.setCapability("browserName","chrome");
-            caps.setCapability("platform","Windows 10");
+            caps.setCapability("platform","macOS Catalina 10.15.3");
             caps.setCapability("version","latest");
            // caps.setCapability("tunnel-identifier","futureItVision");
             driver=new RemoteWebDriver(new URL(URL),caps);
